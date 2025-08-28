765c9faa1ca30a5b82c920390545400851f74d59
avscs <(none)>
2025-08-28 22:50:11 +0530

    added adduser binary to add users to db

---
+++ src/bin/adduser.rs
@@ -0,0 +1,64 @@
+// this is meant to add a user to the database
+use bcrypt::hash;
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+use std::{
+    env,
+    io::{self, Write},
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let db = PgPoolOptions::new().connect(&db_url).await?;
+
+    let bcrypt_cost: u32 = env::var("BCRYPT_COST")
+        .unwrap_or(String::from("10"))
+        .parse()
+        .unwrap_or(10);
+
+    let mut username = String::new();
+    get_response("Enter username", &mut username)?;
+
+    let mut password = String::new();
+    get_response("Enter password", &mut password)?;
+
+    let view_history = loop {
+        print!("View history? [Y/n]: ");
+        io::stdout().flush()?;
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+
+        match input.to_lowercase().trim() {
+            "y" | "yes" | "" => break true,
+            "n" | "no" => break false,
+            _ => {
+                println!("Invalid input");
+                continue;
+            }
+        }
+    };
+
+    let password_hash = hash(&password, bcrypt_cost)?;
+    sqlx::query!(
+        "INSERT INTO users (username, password_hash, view_history) VALUES ($1, $2, $3)",
+        username,
+        password_hash,
+        view_history
+    )
+    .execute(&db)
+    .await?;
+
+    Ok(())
+}
+
+fn get_response(question: &str, output: &mut String) -> Result<(), Box<dyn std::error::Error>> {
+    print!("{}: ", question);
+    io::stdout().flush()?;
+
+    io::stdin().read_line(output)?;
+
+    Ok(())
+}
